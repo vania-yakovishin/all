11fd7fff6f930ceb1565adf2e470ede116004ffe
ivany <ivan.yakovishin2011@gmail.com>
2024-03-02 12:20:43 +0200

    sdsddw

---
+++ dz.py
@@ -0,0 +1,16 @@
+import random
+class NumberEncryptor:
+    def __init__(self):
+        self.number = None
+    def encrypt(self, number):
+        self.number = number
+        self.number += random.randint(1, 10)
+    def decrypt(self):
+        return self.number - random.randint(1, 10)
+if __name__ == "__main__":
+    encryptor = NumberEncryptor()
+    original_number = 42
+    encryptor.encrypt(original_number)
+    print("Зашифроване число:", encryptor.number)
+    decrypted_number = encryptor.decrypt()
+    print("Розшифроване число:", decrypted_number)

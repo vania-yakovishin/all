caf8689bfd08feab12b51f1c8d2666e625741f74
ivany <ivan.yakovishin2011@gmail.com>
2024-03-10 11:02:53 +0200

    my 6 dz

---
+++ 10 berezna yrok 2.py
@@ -0,0 +1,58 @@
+def iter_numbers():
+   for i in range(10):
+       yield i
+iterator = iter_numbers()
+for i in iterator:
+    print(i)
+
+def func_gen():
+    g = 1
+    while True:
+        yield g
+        g += 1
+d = func_gen()
+print(next(d))
+print(next(d))
+
+def func_gen():
+    v = 0
+    while True:
+     yield v
+     v += 1
+f = func_gen()
+print(f)
+
+f= (i for i in range(50))
+print(f)
+print(next(f))
+print(next(f))
+print(next(f))
+
+def test_range():
+    for i in range(0,1):
+        yield i
+i=test_range()
+print(next(i))
+
+def outer_function():
+  x = 10
+  def inner_function():
+    nonlocal x
+    x += 1
+    return x
+  return inner_function
+function=outer_function()
+print(function())
+
+def guard_zero_ (operate):
+   def inner (x, y):
+      if y == 0:
+         print("Cannot divide by 8.")
+         return
+      return operate(x, y)
+   return inner
+@guard_zero
+def divide (x, y):
+   return x/y
+
+print(divide( 5,0))

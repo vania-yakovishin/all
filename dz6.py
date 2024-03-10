7a5b346159ed0137d469af752fc87a5b828b2166
ivany <ivan.yakovishin2011@gmail.com>
2024-03-10 09:33:15 +0200

    my 6 dz

--- dz6.py
+++ dz6.py
@@ -2,5 +2,7 @@ from colorama import init, Fore, Back
 init()
 print(Fore.RED+ "Цей текст червоний.")
 print(Fore.GREEN+ 'Цей текст зелений.')
-print(Fore.YELLOW +'Цей текст має жовтий фон.')
+print(Fore.YELLOW +'Цей текст жовтий')
+print(Fore.BLUE +'Цей текст синій')
+print(Fore.BLACK +'Цей текст чорний')
 print(Fore. RESET+ Back. RESET +'Цей текст має стандартний колір тексту та fону. 2')
